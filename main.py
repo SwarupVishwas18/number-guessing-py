3d905b8e0db63956c667056c8b9740da20423f35
SwarupVishwas18 <swarupvishwas000@gmail.com>
2022-08-15 02:43:49 +0530

    Added colors and abouts

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import random
 import time
-print("NUMBER GUESSING GAME")
+from colorama import Fore
+import normal
+
+normal.printBrand("Number Guessing Game")
 name = input("Enter your name : ")
 time.sleep(2)
+print(Fore.YELLOW)
 print(f"Hey {name}, I am Half Blood Prince, Host of this amazing game..!!")
 print("Rules are simple :-")
 print("I'll think about the number and you will guess it")
@@ -18,9 +22,11 @@ results = {}
 
 
 def quit_prompt(rnd):
+    print(Fore.CYAN)
     yn = input("\n\nDo you want to continue , Y/N ? ")
     if yn.upper() == 'Y':
         rnd += 1
+        print(Fore.YELLOW)
 
         print("\n####################################################################")
         print("-----------------------REFRESHING TABLES----------------------------")
@@ -35,36 +41,47 @@ def quit_prompt(rnd):
         for key, val in results.items():
             print(f"\n\nROUND {key} = {val}")
             time.sleep(1)
+        print(Fore.MAGENTA)
+        time.sleep(2)
         print("Thank You..!!")
-        print("Created By Swarup Deepak Vishwas..!!")
-        time.sleep(10)
-        return rnd, False
+        time.sleep(2)
+        normal.aboutMe()
+        time.sleep(2)
+        normal.quitMe()
+        # time.sleep(10)
+        # return rnd, False
 
 
 control = True
 
 while control:
     guess_count = 0
+    print(Fore.CYAN)
     print("\n\nI have guessed number between 1-25")
     ai_number = random.randint(1, 25)
     while guess_count < 6:
+        print(Fore.CYAN, end='')
         guessed_number = int(input("\nYour guess is : "))
 
         if guessed_number == ai_number:
+            print(Fore.LIGHTGREEN_EX)
             print("\nYOU ARE BULLSEYE..!!")
-            results[rnd] = "W O N . . ! !"
+            results[rnd] = "W O N . . ! ! 🏆🏆"
             rnd, control = quit_prompt(rnd)
 
             guess_count = 10
 
         elif guessed_number < ai_number:
-            print("\nYOU ARE COLD..!!")
+            print(Fore.LIGHTBLUE_EX)
+            print("\nYOU ARE COLD..!! 🥶🥶")
             guess_count += 1
         else:
-            print("\nYOU ARE HOT..!!")
+            print(Fore.LIGHTRED_EX)
+            print("\nYOU ARE HOT..!! 🔥🔥")
             guess_count += 1
 
         if guess_count >= 6 and not(guess_count > 8):
+            print(Fore.RED)
             print("\n\nYou have completed all the chances, So you have failed")
             results[rnd] = "L O S T . . ! !"
             rnd, control = quit_prompt(rnd)
--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup  
-import py2exe
-import random
-import time  
-
-
-setup(console=['main.py'])
